d507c0d184b1e84c888d6bbce90262a07e284054
swagops001 <48081819+swagops001@users.noreply.github.com>
2019-04-04 19:01:28 +1100

    Update main.c
    So I did somethings I don't completely understand so I off to bed

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
-#include <stdin.h> 
+#include <stdio.h>
+
+void Encrpt( char complexer[], int slider); 
 
 int main() { 
+    char complexer[50]; // Complexer is an array of 50 letters  
+    int slider; 
 
-  return 0, 
+    printf("Enter a message to be encrpted: ");
+    scanf("%s", complexer);
+    
+    printf("Enter the amount of Slides to be completed: \n * The more slides the more encrpted your message gets\n");
+    scanf("%d", &slider); 
+    
+    Encrpt(complexer, slider); 
+    
+  return 0;
 } 
+ void Encrpt( char complexer[], int slider){
+     int x = 0; 
+     
+     while (Encrpt [x] != '\0'){
+         if ((Encrpt [x] += slider) >= 65 && (Encrpt [x] += slider)< 90) {
+             Encrpt [x] += slider; 
+        } else{
+            Encrpt [x] += (slider - 25); 
+        }  
+        x++; 
+    }
+    printf("Your ecrpted message is: %s", complexer); 
+}
